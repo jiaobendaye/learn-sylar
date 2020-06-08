5b31d40df417696ae9177113b4a86caf7f8ca198
401-ubuntu <240450828@qq.com>
2020-06-08 11:56:47 +0800

    p25 add mutex on config

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -6,7 +6,9 @@
 #define TEST_YAML_FILE "/home/jiaobendaye/Documents/lab/c++/bzhan-sylar/sylar/bin/conf/test.yml"
 #define LOG_YAML_FILE "/home/jiaobendaye/Documents/lab/c++/bzhan-sylar/sylar/bin/conf/log.yml"
 
-#if 1
+static sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
+
+#if 0
 sylar::ConfigVar<int>::ptr g_int_value_config =
     sylar::Config::Lookup("system.port", (int)8080, "system port");
 
@@ -189,7 +191,7 @@ void test_class() {
         SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) <<  prefix << ": size=" << m.size(); \
     }
 
-    g_person->addListener(10, [](const Person& old_value, const Person& new_value){
+    g_person->addListener([](const Person& old_value, const Person& new_value){
         SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "old_value=" << old_value.toString()
                 << " new_value=" << new_value.toString();
     });
@@ -225,6 +227,12 @@ int main(int argc, char** argv) {
     //test_yaml();
     //test_config();
     //test_class();
-    test_log();
+    //test_log();
+    sylar::Config::Visit([](sylar::ConfigVarBase::ptr var){
+        SYLAR_LOG_INFO(g_logger) << "name =" << var->getName()
+                    << " description =" << var->getDescription()
+                    << " type =" << var->getTypeName()
+                    << " value =" << var->toString();
+    });
     return 0;
 }
